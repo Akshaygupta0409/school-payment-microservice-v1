5a7ebb0da1136b15df47a7a54843b956fa298b0e
akshay kumar gupta <guptaakshay0409@gmail.com>
2025-04-29 19:45:36 +0530

    deuging deployment error

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from '../utils/axiosConfig.js';
+
 import NeonGridBackground from './NeonGridBackground';
 
 export default function Login({ setIsAuthenticated }) {
